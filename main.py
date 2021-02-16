04794cdafa076e44896a99d7a80a9fbe53bd3fc9
Kenny O <kenny_oliver@icloud.com>
2021-02-16 13:05:58 +0000

    Working well now!

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-stats_file = open("stats.txt", 'r')
+stats_file = open("stats.txt",'r')
 for record in stats_file:
   values = record.split(',')
-  global books_in_stock = values[0]
-  global books_lended = values[1]
+  books_in_stock = int(values[0])
+  books_lended = int(values[1])
 
-def book_return(quantity):
-  books_in_stock += quantity
+def book_return(books_in_stock,books_lended,quantity):
+  new_stock_value = books_in_stock - quantity
   books_lended -= quantity
+  vals = [new_stock_value, books_lended]
+  return vals
 #====================
-def book_lend(quantity):
-  books_in_stock -= quantity
+def book_lend(books_in_stock,books_lended,quantity):
+  new_stock_value = books_in_stock - quantity
   books_lended += quantity
+  vals = [new_stock_value, books_lended]
+  return vals
 #====================
 def view_stats():
   print(f"Books in stock:\t{books_in_stock}".format(books_in_stock))
@@ -27,14 +31,20 @@ def menu():
     quantity = int(input("Enter the quantity of books to be returned\n--> "))
     while type(quantity) != int:
       quantity = int(input("Enter the quantity of books to be returned\n--> "))
-    book_return(quantity)
+    new_stocking = book_return(books_in_stock,books_lended,quantity)[0]
+    new_lending = book_return(books_in_stock,books_lended,quantity)[1]
   elif option == 'L':
     quantity = int(input("Enter the quantity of books to be lended\n--> "))
     while type(quantity) != int:
       quantity = int(input("Enter the quantity of books to be lended\n--> "))
-    book_lend(quantity)
+    new_stocking = book_return(books_in_stock,books_lended,quantity)[0]
+    new_lending = book_return(books_in_stock,books_lended,quantity)[1]
   elif option == 'S':
     view_stats()
+  
+  stats_file = open("stats.txt",'w')
+  stats_file.write(str(new_stocking) + ',' + str(new_lending))
+  stats_file.close()
 
 
 # main program
