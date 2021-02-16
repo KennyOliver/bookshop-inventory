e60730314485b6dc1aa695c6ab401612b441255e
Kenny O <kenny_oliver@icloud.com>
2021-02-16 13:53:13 +0000

    [+] Fixed bug which cleared stats.txt every time “View Stats” option was chosen

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-stats_file = open("stats.txt",'r')
-for record in stats_file:
-  values = record.split(',')
-  books_in_stock = int(values[0])
-  books_lended = int(values[1])
-
 def book_return(books_in_stock,books_lended,quantity):
   new_stock_value = books_in_stock - quantity
   books_lended -= quantity
@@ -16,11 +10,11 @@ def book_lend(books_in_stock,books_lended,quantity):
   vals = [new_stock_value, books_lended]
   return vals
 #====================
-def view_stats():
+def view_stats(books_in_stock,books_lended):
   print(f"Books in stock:\t{books_in_stock}".format(books_in_stock))
   print(f"Books lended:\t{books_lended}".format(books_lended))
 #====================
-def menu():
+def menu(books_in_stock,books_lended):
   print("Pick action:\n\t[R] Return\n\t[L] Lend\n\t[S] View Stats")
   option = input("--> ").upper()
   
@@ -33,20 +27,32 @@ def menu():
       quantity = int(input("Enter the quantity of books to be returned\n--> "))
     new_stocking = book_return(books_in_stock,books_lended,quantity)[0]
     new_lending = book_return(books_in_stock,books_lended,quantity)[1]
+    write_data(new_stocking,new_lending)
+
   elif option == 'L':
     quantity = int(input("Enter the quantity of books to be lended\n--> "))
     while type(quantity) != int:
       quantity = int(input("Enter the quantity of books to be lended\n--> "))
     new_stocking = book_return(books_in_stock,books_lended,quantity)[0]
     new_lending = book_return(books_in_stock,books_lended,quantity)[1]
+    write_data(new_stocking,new_lending)
+
   elif option == 'S':
-    view_stats()
-  
+    view_stats(books_in_stock,books_lended)
+#====================
+def write_data(new_stocking,new_lending):
   stats_file = open("stats.txt",'w')
   stats_file.write(str(new_stocking) + ',' + str(new_lending))
   stats_file.close()
 
 
+
 # main program
-menu()
+stats_file = open("stats.txt",'r')
+for record in stats_file:
+  values = record.split(',')
+  books_in_stock = int(values[0])
+  books_lended = int(values[1])
+
+menu(books_in_stock,books_lended)
 stats_file.close()
